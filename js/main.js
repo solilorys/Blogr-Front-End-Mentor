6f544958f0d023b7e00498b7c0d0f7c9d8b483ad
Laryssa Brilhante <94483769+solilorys@users.noreply.github.com>
2022-09-01 12:02:03 -0300

    Finalização do desafio do Frontend Mentor

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const btnMobile = document.querySelector(".cabecalho__botao");
-const accordionHeader = document.querySelector(".accordion__header");
+const accordionContainer = document.querySelectorAll(".accordion__container");
 
 function toggleCabecalho() {
     const nav = document.querySelector(".cabecalho__nav");
     nav.classList.toggle("active");
 }
 
-function toggleAccordion() {
-    const accordionContainer = document.querySelector(".accordion__container");
-    accordionContainer.classList.toggle("active");
+for (i = 0; i < accordionContainer.length; i++) {
+    accordionContainer[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+    });
 }
 
 btnMobile.addEventListener("click", toggleCabecalho);
-accordionHeader.addEventListener("click", toggleAccordion);
